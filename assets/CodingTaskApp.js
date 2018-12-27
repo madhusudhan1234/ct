41c3581bffcf8d05ce97a84349e0ede81cdeffcf
Madhu Sudhan <madhusudhansubedi4@gmail.com>
2018-12-28 00:07:15 +0545

    Starter for the react is created - [x] Laravel mix is implemented for the assets bundling

---
+++ assets/CodingTaskApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class CodingTaskApp extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Starter Component</h1>
+      </div>
+    );
+  }
+}
+
+export default CodingTaskApp;
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+const mix = require('laravel-mix');
+
+mix.react('assets/app.js', 'app/public/js');
