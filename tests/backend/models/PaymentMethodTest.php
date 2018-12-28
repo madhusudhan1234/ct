4adddd54868142b10699b4d6d026ce785d847bee
Madhu Sudhan <madhusudhansubedi4@gmail.com>
2018-12-29 01:07:27 +0545

    PaymentMethods Model is tested

---
+++ tests/backend/models/PaymentMethodTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Models\PaymentMethod;
+
+class PaymentMethodTest extends TestCase {
+
+    public function testCanReturnPaymentMethods() {
+        $this->assertCount(2, PaymentMethod::getPaymentMethods());
+    }
+}
--- tests/backend/models/ProductTest.php
+++ tests/backend/models/ProductTest.php
@@ -3,7 +3,7 @@
 use App\Models\Product;
 use PHPUnit\Framework\TestCase;
 
-class ProductTest extends TestCase{
+class ProductTest extends TestCase {
 
     public function testCanReturnAllProducts() {
         $this->assertCount(2, Product::getProducts());
