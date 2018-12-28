1f635b65d1a3acd865706c3e31703519e686c5e1
Madhu Sudhan <madhusudhansubedi4@gmail.com>
2018-12-29 00:55:47 +0545

    Removed $products

--- tests/backend/models/ProductTest.php
+++ tests/backend/models/ProductTest.php
@@ -1,23 +1,7 @@
 <?php
-declare(strict_types=1);
 
 use App\Models\Product;
 use PHPUnit\Framework\TestCase;
-
-$products = [
-    [
-        "id" => "1",
-        "name" => "Tshirt",
-        "price" => "50",
-        "currency" => "USD",
-    ],
-    [
-        "id" => "2",
-        "name" => "Monitor",
-        "price" => "100",
-        "currency" => "USD",
-    ]
-];
 
 class ProductTest extends TestCase{
 
